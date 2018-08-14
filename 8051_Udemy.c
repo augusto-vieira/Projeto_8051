4568722a319e96114649407a5bf3ada4bdb8afda
AugustoVieira <augustovieira18@gmail.com>
2018-08-14 11:53:53 -0300

    Soma P0+P1 com valor no LCD

--- 8051_Udemy.c
+++ 8051_Udemy.c
@@ -1,40 +1,45 @@
+
+      /*
+       enviamos dados para o LCD no formato ASCII
+       Vou ligar o LCD à porta 2
+       Mas não há necessidade de configurar P2 como saída, isso é feito automaticamente pela função lcd_init()
+       Mas não há necessidade de configurar P2 como saída, isso é feito automaticamente pela função lcd_init()
+       Esta função inicializa o LCD no modo de 4 bits
+       A função lcd_cmd() é usada para enviar comandos para o LCD
+       Lcd_Out() é a função de biblioteca usada para exibir algumas cadeias na linha e coluna especificadas
+      
+       Podemos adicionar algum atraso, caso contrário, a exibição será atualizada em alta frequência
+       Acrescentei 500 milissegundos de atraso para que a exibição seja atualizada duas vezes por segundo.
+       
+     */
+// lcd pinout settings
+sbit LCD_RS at P2_0_bit;
+sbit LCD_EN at P2_1_bit;
+sbit LCD_D7 at P2_5_bit;
+sbit LCD_D6 at P2_4_bit;
+sbit LCD_D5 at P2_3_bit;
+sbit LCD_D4 at P2_2_bit;
+
+
 void main() {
 
-     /*
-      vamos configurar P0 e P1 como entrada,enquanto P2 e P3 serão configurados como saída,
-      e vamos somar os valores do P0 com P1 e o resultado deverá ser exibido em 7 segmentos.
-
-       Para fazer isso, primeiro precisamos converter nossos resultados do formato decimal para o formato BCD.
-       
-      Agora podemos conectar esses 7 segmentos com a porta 2 e a porta 3, pois essas portas serão configuradas como saídas
-      vamos adicionar P0 e P1 e armazenar o resultado na variável ----"sum" ----
-      a variável 'sum' contém o resultado em formato decimal, mas 7 segmentos precisam de dados no formato BCD
-      
-      Então, primeiro temos que converter o resultado do decimal em BCD, precisamos de outra variável para armazenar dados no formato BCD
-      Dec2Bcd16() é função de biblioteca mikroC integrada que converte decimal em números BCD de 16 bits
-        A variável "sumInBcdFormat" vai receber o valor convertido para BCD 16bits
-        
-        Dec2Bcd16() = 16bits ---> (0000000000000000) 1111-1111.0000-0000
-         P2 = 11111111 (8bits) gravo uma parte em P2
-         P3 = 00000000   "      e o resto em P3
-
-     */
-
-     unsigned int sum, sumInBcdFormat;            // declarando 2 variáveis
-
+     unsigned int sum;                            // declarando 2 variáveis
+     char txt[6];
+    
      P0 = 0xff;                                   // 1111-1111  PORT1 (1-Input)
      P1 = 0x00;                                   // 0000-0000  PORT2 (0-Output)
-     P2 = 0x00;                                   // Port3 como saída
-     P3 = 0x00;                                   // Port4  "   saída
+     
+     Lcd_init();
+     delay_ms(10);
+     lcd_cmd(_LCD_CURSOR_OFF);                    // lcd_cmd() ---> função para fazer comandos no LCD
+     lcd_out(1,1,"****************");
+     
      
      while(1){
          sum = P0 + P1;                           // Soma o Port0 com Port1;
-         sumInBcdFormat = Dec2Bcd16(sum);         // Dec2Bcd16(sum) ==> converte o valor da resposta em BCD de 16 bits
-         P2 = sumInBcdFormat;                     // gravamos apenas 8 bits o resto vai para P3
-         P3 = sumInBcdFormat >> 8;                // deslocamos à direita 'sumInBcdFormat'  8bits
-         
-         
+         wordToStr(sum, txt);                     // Converte a palavra para uma string
+         lcd_out(2,1,"P0+P1=");                   // Vamos converter nosso resultado de decimal para string e passá-lo para o LCD
+         lcd_out(2,7,txt);
+         delay_ms(500);
      }
-     
-
 }
