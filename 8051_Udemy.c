eb4cb985dd79697fdfc7c30c06842a966dbb59c6
AugustoVieira <augustovieira18@gmail.com>
2018-08-17 14:02:53 -0300

    Duty cycle de 50%

--- 8051_Udemy.c
+++ 8051_Udemy.c
@@ -1,17 +1,4 @@
 
-      /*
-       enviamos dados para o LCD no formato ASCII
-       Vou ligar o LCD à porta 2
-       Mas não há necessidade de configurar P2 como saída, isso é feito automaticamente pela função lcd_init()
-       Mas não há necessidade de configurar P2 como saída, isso é feito automaticamente pela função lcd_init()
-       Esta função inicializa o LCD no modo de 4 bits
-       A função lcd_cmd() é usada para enviar comandos para o LCD
-       Lcd_Out() é a função de biblioteca usada para exibir algumas cadeias na linha e coluna especificadas
-      
-       Podemos adicionar algum atraso, caso contrário, a exibição será atualizada em alta frequência
-       Acrescentei 500 milissegundos de atraso para que a exibição seja atualizada duas vezes por segundo.
-       
-     */
 // lcd pinout settings
 sbit LCD_RS at P2_0_bit;
 sbit LCD_EN at P2_1_bit;
@@ -20,26 +7,48 @@ sbit LCD_D6 at P2_4_bit;
 sbit LCD_D5 at P2_3_bit;
 sbit LCD_D4 at P2_2_bit;
 
-
+//Gerar uma onda quadrada com duty cycle de 50%
+void squareWave(void){
+     while(1){
+        while(!TCON.TF0);
+        TCON.TF0 = 0;
+        P3.B7 = 1;
+        while(!TCON.TF0);
+        TCON.TF0 = 0;
+        P3.B7 = 0;
+     }
+}
 void main() {
 
-     unsigned int sum;                            // declarando 2 variáveis
+     unsigned int sum;
      char txt[6];
     
      P0 = 0xff;                                   // 1111-1111  PORT1 (1-Input)
      P1 = 0x00;                                   // 0000-0000  PORT2 (0-Output)
+     UART1_INIT(9600);
+     PCON.SMOD = 0;
+     
+     TMOD = 0x02;
+     SCON = 0x50;                                 // 8-bit, rx enable
+     TL0  = 0x00;                                 // Lower Byte
+     TH0  = 0xD2;                                 // Upper Byte
+     TCON.TR0  = 1;                               // Run timer
+     squareWave();
      
      Lcd_init();
      delay_ms(10);
      lcd_cmd(_LCD_CURSOR_OFF);                    // lcd_cmd() ---> função para fazer comandos no LCD
      lcd_out(1,1,"****************");
-     
+     UART1_WRITE_TEXT("Usando o UART");
      
      while(1){
-         sum = P0 + P1;                           // Soma o Port0 com Port1;
+         sum = P0 + P1;
          wordToStr(sum, txt);                     // Converte a palavra para uma string
-         lcd_out(2,1,"P0+P1=");                   // Vamos converter nosso resultado de decimal para string e passá-lo para o LCD
+         lcd_out(2,1,"P0+P1=");
          lcd_out(2,7,txt);
+         UART1_WRITE_TEXT("P0+P1=");
+         UART1_WRITE_TEXT(txt);
+         UART1_WRITE_TEXT("\r\n");
          delay_ms(500);
      }
 }
